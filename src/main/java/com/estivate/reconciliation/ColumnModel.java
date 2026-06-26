c6cda878f5f17641b480acf810a4d0368703e8bb
jerome <jerome.manchon@gadz.org>
2026-06-26 23:26:17 +0200

    minor fix

--- src/main/java/com/estivate/reconciliation/ColumnModel.java
+++ src/main/java/com/estivate/reconciliation/ColumnModel.java
@@ -1,9 +1,5 @@
 package com.estivate.reconciliation;
 
-import java.util.Optional;
-
-import com.estivate.util.Nullable;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
